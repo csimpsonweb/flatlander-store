20ef9a373dbe4e3878ea05c4c6c097c33a389c6a
Randy Tolentino <makertolentino@gmail.com>
2014-07-25 03:41:48 -0500

    added new gem to gems array of objects, iterated through gems using directive in div

--- js/app.js
+++ js/app.js
@@ -3,13 +3,24 @@
 	var app = angular.module('store', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
+	var gems = [
+	{
 		name: 'Dodecahedron',
 		price: 2.95,
-		description: 'This gem is awesome'
+		description: 'This gem is awesome',
+		canPurchase: true,
+		soldOut: false
+	},
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: 'This gem is super sharp',
+		canPurchase: true,
+		soldOut: false
 	}
+	];
 
 })();
