3d81b47d2d29f536fa19d37494f63d2b57b7b436
Randy Tolentino <makertolentino@gmail.com>
2014-08-15 08:00:30 -0500

    styling forms

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
 
 	var gems = [
 	{
-		name: 'Dope Diamonds',
+		name: 'Dope Diamond',
 		price: 82.95,
 		description: "Lorizzle ipsizzle dolor sit dang, we gonna chung that's the shizzle shut the shizzle up. Nullam tellivizzle bling bling, aliquet volutpizzle, suscipit break it down, gravida.",
 		specification: "Phasellizzle fo shizzle my nizzle crunk. Curabitur non velizzle the bizzle pede crackalackin facilisizzle. Bow wow wow rizzle nulla, iaculizzle fo shizzle, break.",
@@ -45,7 +45,7 @@
 		]
 	},
 	{
-		name: 'Rare Rubies',
+		name: 'Rare Ruby',
 		price: 95.95,
 		description: "Maecenas quis metus izzle fo shizzle my nizzle yo gangsta. Break yo neck, yall dope viverra fo. Curabitizzle sollicitudin boom shackalack quizzle purus.",
 		specification: "Lorizzle ipsizzle dolor sit dang, we gonna chung that's the shizzle shut the shizzle up. Nullam tellivizzle bling bling, aliquet volutpizzle, suscipit break it down, gravida.",
